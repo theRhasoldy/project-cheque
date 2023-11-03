fc19ef57c2898189a4daa902dfa0e0f451391fc9
Nour El Din Shady <rhasoldy@gmail.com>
2023-11-03 13:53:08 +0200

    chore(project): initial project setup and added eslint and prettier

---
+++ prettier.config.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  semi: true,
+  singleQuote: false,
+  trailingComma: "es5",
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+};
