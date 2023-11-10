767fd825638395d6bb97211b22930c31878f708a
Nour El Din Shady <rhasoldy@gmail.com>
2023-11-11 01:19:08 +0200

    feat(auth page): added general layout

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,13 +1,30 @@
 import { ReactNode } from "react";
 import TabBar from "./TabBar";
 import InvalidMessage from "./InvalidMessage";
+import Image from "next/image";
 
 const RegisterPage = ({ children }: { children: ReactNode }) => {
   return (
-    <div>
-      <TabBar />
-      {children}
-      <InvalidMessage />
+    <div className="flex flex-col-reverse md:grid lg:grid grid-cols-2">
+      <div className="flex flex-col items-center">
+        <div>
+          <TabBar />
+        </div>
+        <div className="h-full w-full flex items-center justify-center">
+          {children}
+        </div>
+        <InvalidMessage />
+      </div>
+      <div className="relative h-screen">
+        <Image
+          className="object-cover rounded-[3rem] p-4"
+          src={"/img/auth-cover.jpg"}
+          priority
+          fill
+          alt="Group of friends splitting the check"
+          sizes="(max-width: 768px) 50vw, (max-width: 1200px) 50vw, 33vw"
+        />
+      </div>
     </div>
   );
 };
