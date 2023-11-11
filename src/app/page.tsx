4ddbb9889921a6439d51016c3e4829aef783de8a
Nour El Din Shady <rhasoldy@gmail.com>
2023-11-12 00:17:32 +0200

    feat(home): fetch room cards in server side

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,41 @@
 import CreateRoomButton from "@/components/CreateRoomButton";
+import RoomCard from "@/components/RoomCard";
 import { supabaseServer } from "@/lib/api/supabase";
 import Typography from "@mui/material/Typography";
 
+export const revalidate = 0;
+
 export default async function Home() {
-  const userAuth = await supabaseServer.auth.getUser();
-  const { data } = await supabaseServer
+  const userAuth = await supabaseServer?.auth?.getUser();
+
+  const { data: userData } = await supabaseServer
     .from("users")
     .select("username")
-    .eq("id", String(userAuth.data.user?.id));
+    .eq("id", String(userAuth?.data?.user?.id));
+
+  const { data: roomsData, error } = await supabaseServer
+    .from("rooms")
+    .select()
+    .eq("owner", "6a9bd2ec-7703-43ec-877e-99b30ffefded");
+
+  error && console.log(error.message);
+  console.log(roomsData);
 
   return (
-    <main className="flex flex-col w-full h-screen justify-center items-center">
-      <div className="w-1/2">
+    <main className="flex flex-col w-full h-screen items-center">
+      <div className="mt-[8vw] w-1/2">
         <Typography className="flex" variant="h1">
-          Hello {data?.at(0)?.username ?? "User"},
+          Hello {userData?.at(0)?.username ?? "User"},
         </Typography>
         <Typography className="flex" variant="h2">
           Hope your meal was as delicious as you 🤤
         </Typography>
-        <CreateRoomButton />
+        <div className="flex flex-col gap-8">
+          <CreateRoomButton />
+          {roomsData?.map((room) => (
+            <RoomCard key={room?.id} name={room?.name} />
+          ))}
+        </div>
       </div>
     </main>
   );
