897ecf874d09682b5397d41e700cb311239e87ab
Nour El Din Shady <rhasoldy@gmail.com>
2023-11-11 15:45:52 +0200

    refactor(MUI theme): made a defaults object and spread it in themes

--- src/lib/theme.ts
+++ src/lib/theme.ts
@@ -1,62 +1,51 @@
 import { Montserrat } from "next/font/google";
 import resolveConfig from "tailwindcss/resolveConfig";
 import config from "../../tailwind.config";
-import { createTheme } from "@mui/material";
-import { TypographyOptions } from "@mui/material/styles/createTypography";
+import { ThemeOptions, createTheme } from "@mui/material";
 import LinkBehaviour from "@/components/Link";
 
 const tailwindConfig = resolveConfig(config);
 const montserrat = Montserrat({ subsets: ["latin"] });
 
-const fontFamily: TypographyOptions = {
-  fontFamily: [
-    montserrat.style.fontFamily,
-    "-apple-system",
-    "BlinkMacSystemFont",
-    "'Segoe UI'",
-    "Roboto",
-    "'Helvetica Neue'",
-    "Arial",
-    "sans-serif",
-    "'Apple Color Emoji'",
-    "'Segoe UI Emoji'",
-    "'Segoe UI Symbol'",
-  ].join(","),
-  h1: {
-    fontSize: tailwindConfig.theme.fontSize["4xl"],
-    fontWeight: 600,
-    letterSpacing: -0.75,
+const themeDefaults: ThemeOptions = {
+  typography: {
+    fontFamily: [
+      montserrat.style.fontFamily,
+      "-apple-system",
+      "BlinkMacSystemFont",
+      "'Segoe UI'",
+      "Roboto",
+      "'Helvetica Neue'",
+      "Arial",
+      "sans-serif",
+      "'Apple Color Emoji'",
+      "'Segoe UI Emoji'",
+      "'Segoe UI Symbol'",
+    ].join(","),
+    h1: {
+      fontSize: tailwindConfig.theme.fontSize["4xl"],
+      fontWeight: 600,
+      letterSpacing: -0.75,
+    },
+    h2: {
+      fontSize: tailwindConfig.theme.fontSize["3xl"],
+      fontWeight: 600,
+      letterSpacing: -0.75,
+    },
+    h3: {
+      fontSize: tailwindConfig.theme.fontSize["2xl"],
+      letterSpacing: -0.75,
+    },
+    body1: {
+      fontSize: tailwindConfig.theme.fontSize.base,
+    },
+    body2: {
+      fontSize: tailwindConfig.theme.fontSize.sm,
+    },
+    button: {
+      textTransform: "none",
+    },
   },
-  h2: {
-    fontSize: tailwindConfig.theme.fontSize["3xl"],
-    fontWeight: 600,
-    letterSpacing: -0.75,
-  },
-  h3: {
-    fontSize: tailwindConfig.theme.fontSize["2xl"],
-    letterSpacing: -0.75,
-  },
-  body1: {
-    fontSize: tailwindConfig.theme.fontSize.base,
-  },
-  body2: {
-    fontSize: tailwindConfig.theme.fontSize.sm,
-  },
-  button: {
-    textTransform: "none",
-  },
-};
-
-const colors = tailwindConfig.theme?.colors;
-
-export const lightTheme = createTheme({
-  palette: {
-    mode: "light",
-    background: colors.light.background,
-    primary: colors.light.primary,
-    secondary: colors.light.secondary,
-  },
-  typography: fontFamily,
   shape: {
     borderRadius: 9,
   },
@@ -72,17 +61,26 @@ export const lightTheme = createTheme({
       },
     },
   },
+};
+
+const colors = tailwindConfig.theme?.colors;
+
+export const lightTheme = createTheme({
+  ...themeDefaults,
+  palette: {
+    mode: "light",
+    background: colors.light.background,
+    primary: colors.light.primary,
+    secondary: colors.light.secondary,
+  },
 });
 
 export const darkTheme = createTheme({
+  ...themeDefaults,
   palette: {
     mode: "dark",
     background: colors.dark.background,
     primary: colors.dark.primary,
     secondary: colors.dark.secondary,
-  },
-  typography: fontFamily,
-  shape: {
-    borderRadius: 9,
   },
 });
