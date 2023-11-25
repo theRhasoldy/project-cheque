341d7135dbd39a06e3163742c26b65d6ad09373a
Nour El Din Shady <rhasoldy@gmail.com>
2023-11-25 16:22:17 +0200

    feat(room): update orders list when data is deleted in backend

--- src/app/[room]/RealtimeOrders.tsx
+++ src/app/[room]/RealtimeOrders.tsx
@@ -2,20 +2,44 @@
 
 import { Database, Order } from "@/lib/api/databaseTypes";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
 const RealtimeOrders = ({ initialOrders }: { initialOrders: Order[] }) => {
   const [roomOrders, setRoomOrders] = useState<Order[]>(initialOrders);
+  const router = useRouter();
 
   const supabaseClient = createClientComponentClient<Database>();
 
+  // Listen to INSERT in orders table and add to the frontend side
   useEffect(() => {
     const channel = supabaseClient
       .channel("orders")
       .on(
         "postgres_changes",
         { event: "INSERT", schema: "public", table: "orders" },
-        (payload) => setRoomOrders((prev) => [...prev, payload.new as Order])
+        (payload) => {
+          setRoomOrders((prev) => [...prev, payload.new as Order]);
+        }
+      )
+      .subscribe();
+
+    return () => {
+      supabaseClient.removeChannel(channel);
+    };
+  }, [supabaseClient]);
+
+  // Listen to DELETE in orders table and update the list in the frontend side
+  useEffect(() => {
+    const channel = supabaseClient
+      .channel("orders")
+      .on(
+        "postgres_changes",
+        { event: "DELETE", schema: "public", table: "orders" },
+        ({ old }) =>
+          setRoomOrders((prev) =>
+            prev.filter((prevOrder) => prevOrder.id !== old.id)
+          )
       )
       .subscribe();
 
