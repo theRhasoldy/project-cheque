6dbfd22a6f60307da70b6ccc43b91fd7869f72c9
Nour El Din Shady <rhasoldy@gmail.com>
2023-11-25 14:08:29 +0200

    feat(room): realtime listen to INSERT events in the table and update the list

--- src/app/[room]/RealtimeOrders.tsx
+++ src/app/[room]/RealtimeOrders.tsx
@@ -1,9 +1,36 @@
 "use client";
 
-import { Order } from "@/lib/api/databaseTypes";
+import { Database, Order } from "@/lib/api/databaseTypes";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { useEffect, useState } from "react";
 
 const RealtimeOrders = ({ initialOrders }: { initialOrders: Order[] }) => {
-  return <pre>{JSON.stringify(initialOrders)}</pre>;
+  const [roomOrders, setRoomOrders] = useState<Order[]>(initialOrders);
+
+  const supabaseClient = createClientComponentClient<Database>();
+
+  useEffect(() => {
+    const channel = supabaseClient
+      .channel("orders")
+      .on(
+        "postgres_changes",
+        { event: "INSERT", schema: "public", table: "orders" },
+        (payload) => setRoomOrders((prev) => [...prev, payload.new as Order])
+      )
+      .subscribe();
+
+    return () => {
+      supabaseClient.removeChannel(channel);
+    };
+  }, [supabaseClient]);
+
+  return (
+    <>
+      {roomOrders.map((order) => (
+        <p key={order.id}>{order.name}</p>
+      ))}
+    </>
+  );
 };
 
 export default RealtimeOrders;
